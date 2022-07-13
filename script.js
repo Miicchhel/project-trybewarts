453f09142149d3e3baed032eaeacf22060a6ad70
JohnnyNWT <jooaaooskt@gmail.com>
2022-07-13 20:11:03 -0300

    resolvendo conflito

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btnEntrar = document.getElementById('entrar');
 const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('senha');
+const checkEnviar = document.getElementById('agreement');
+const btnEnviar = document.getElementById('submit-btn');
 
 function validaLogin(event) {
   event.preventDefault();
@@ -11,4 +13,16 @@ function validaLogin(event) {
   }
 }
 
+function agreeForm(event) {
+    if (event.target.value === 'on') {
+        btnEnviar.disabled = false;
+    } else {
+        btnEnviar.disabled = true;
+    }
+    console.log(!event.target.value);
+}
+
 btnEntrar.addEventListener('click', validaLogin);
+checkEnviar.addEventListener('click', agreeForm)
+
+
